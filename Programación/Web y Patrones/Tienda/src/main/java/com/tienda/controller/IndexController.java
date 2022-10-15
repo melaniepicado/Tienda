5e6c75a21626940ff9be4198280f4c13415c6ec4
melan <melan@192.168.1.102>
2022-10-15 17:40:44 -0600

    Semana 5 | video 03 | Nuevo Cliente. Creación de formulario.

--- Programación/Web y Patrones/Tienda/src/main/java/com/tienda/controller/IndexController.java
+++ Programación/Web y Patrones/Tienda/src/main/java/com/tienda/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.tienda.controller;
 
+import com.tienda.domain.Cliente;
 import com.tienda.service.ClienteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -22,5 +24,16 @@ public class IndexController {
                 
         model.addAttribute("clientes", clientes);
         return "index";
+    }
+    
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "ModificarCliente";
+    }
+    
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/";
     }
 }
