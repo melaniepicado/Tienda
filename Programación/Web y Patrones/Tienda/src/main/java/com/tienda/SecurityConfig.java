f2447a45869a4783e4ed3b7a737b69e465044190
melaniepicado <melan@192.168.1.105>
2022-12-01 18:55:25 -0600

    Semana 10 | video 01 | Proceso de LogOut: Usuarios y código en pantilla

---
+++ Programación/Web y Patrones/Tienda/src/main/java/com/tienda/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.tienda;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    //El siguiente método funciona para hacer la auttenticación del usuario
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("juan")
+                    .password("{noop}123")
+                    .roles("ADMIN","VENDEDOR","USER")
+                .and()
+                .withUser("rebeca")
+                    .password("{noop}123")
+                    .roles("VENDEDOR","USER")
+                .and()
+                .withUser("pedro")
+                    .password("{noop}123")
+                    .roles("USER");
+    }
+}
--- Programación/Web y Patrones/Tienda/src/main/java/com/tienda/WebConfig.java
+++ Programación/Web y Patrones/Tienda/src/main/java/com/tienda/WebConfig.java
@@ -1,7 +1,6 @@
 package com.tienda;
 
 import java.util.Locale;
-import org.apache.tomcat.util.descriptor.LocalResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
